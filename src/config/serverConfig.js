e11bbb8f90cb9fbb352e651809d823ca330152eb
Yash simejiya <yashsoni48678@gmail.com>
2023-01-29 18:13:51 +0530

    Fixed Some errors

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 const dotenv = require("dotenv");
 dotenv.config()
 
 module.exports = {
     PORT: process.env.PORT
-=======
-const dotenv = require("dotenv");
-dotenv.config()
-
-module.exports = {
-    PORT: process.env.PORT
->>>>>>> 264f2b817e589af64aae45e64ec86aba29d51191
 }
