bb4c0e8a918a754fbbaaff081fc534fb590979e5
Yash simejiya <yashsoni48678@gmail.com>
2023-02-01 19:19:55 +0530

    Revert "Added Airports model"
    This reverts commit edd846b0dd415ee07229f2781b3d1e4ffc27457f.

--- src/models/city.js
+++ src/models/city.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.Airport)
     }
   }
   City.init({
